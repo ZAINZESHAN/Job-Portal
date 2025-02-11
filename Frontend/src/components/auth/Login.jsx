6966b263731d5d8eedc26c7eec4190279e589545
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2025-02-12 04:09:35 +0500

    ok

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -1,10 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../shared/Navbar'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
+import { RadioGroup } from '../ui/radio-group'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  }
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    if(input.file){
+      formData.append("file", input.file);
+    }
+    try {
+      const res = await axios.post(`${}`)
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div>
-      Login
-      cndvxc vjkfn
+      <Navbar />
+      <div className='flex item-center justify-center max-w-7xl mx-auto'>
+        <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+          <h1 className='font-bold text-xl mb-5'>Login</h1>
+          <div className='my-2'>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="zain@gmail.com"
+            />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className='flex items-center gap-4 my-5'>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === 'student'}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === 'recruiter'}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button type='submit' className='w-full my-4 bg-blue hover:bg-blue-hover rounded text-white'>Login</Button>
+          <span className='text-sm'>Don't have an account? <Link to="/signup" className="text-blue-600" >Sign Up</Link></span>
+        </form>
+      </div>
     </div>
   )
 }
